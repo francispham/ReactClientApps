980a4d7dc5a78a9bc9291bbf763cfdc93d3ab95d
Francis <francispham89@gmail.com>
2018-03-09 16:27:19 -0800

    Redirect to New Product Show Page

--- awesome-answers-react/src/components/QuestionNewPage.js
+++ awesome-answers-react/src/components/QuestionNewPage.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
 import QuestionForm from './QuestionForm';
+import { Question } from '../lib/requests';
 
 class QuestionNewPage extends Component {
+  constructor (props) {
+    super(props);
+    this.createQuestion = this.createQuestion.bind(this);
+  }
+
+  createQuestion (questionParams) {
+    Question
+      .create(questionParams)
+      .then(data => {
+        // const id = data.id
+        const { id } = data;
+
+        // Components rendered by the <Route /> component
+        // gain access to a .history than can be used to manipulate
+        // history. Using allows to redirect a user to
+        // a different rendering whichever component is there.
+        this.props.history.push(`/questions/${id}`);
+      })
+  }
+
   render () {
-    return(
+    return (
       <main
         className="QuestionNewPage"
         style={{margin: '0 1rem'}}
-        >
-          <h1>New Question</h1>
-          <QuestionForm />
-        </main>
-
-    )
+      >
+        <h1>New Question</h1>
+        <QuestionForm
+          onSubmit={this.createQuestion}
+        />
+      </main>
+    );
   }
 }
 
